6d50fe1fb483b0ae2dd114e7f69b38b35b565df3
bhavnapathania <pathaniabhavna1998@gmail.com>
2024-08-01 15:43:53 +0530

    hero section done

--- src/app/components/heroSection/index.jsx
+++ src/app/components/heroSection/index.jsx
@@ -2,15 +2,34 @@ import React from "react";
 
 function HeroSection() {
   return (
-    <section className="flex justify-between items-center">
-      <div className="w-[50%]">
+    <section
+      className="flex justify-between items-center h-[80vh] relative bg-center"
+      style={{
+        backgroundImage: 'url("/images/others/bgVisuals.png")',
+        backgroundPosition: "left",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <div className="w-[50%] relative flex flex-col gap-6">
+        <img
+          src="/images/icons/dice.svg"
+          alt="Dice"
+          className="w-20 h-20 absolute top-[-50%] right-10 "
+        />
         <h2 className="text-6xl font-extralight">
           We design <span className="text-orange-500">experiences</span> that
           move businesses to the{" "}
           <span className="text-orange-500">future.</span>
         </h2>
+        <div className="absolute top-[-40%] transform rotate-12 bg-orange-500 text-black rounded-xl px-4 py-2 text-xs font-bold">
+          YOUR IDEA STARTS HERE
+        </div>
       </div>
-      <img className="w-[45%]" src="/images/others/hero.png" alt="" />
+      <img
+        className="w-[45%]"
+        src="/images/others/hero.png"
+        alt="Main Graphic"
+      />
     </section>
   );
 }
