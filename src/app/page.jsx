17f616d35fdd784e9c5431144523ebd432cccb86
bhavnapathania <pathaniabhavna1998@gmail.com>
2024-08-03 17:46:48 +0530

    updated slider images

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -28,12 +28,39 @@ const items = [
   { id: 7, category: "Dashboard", src: "/images/categories/dashboard.png" },
   { id: 8, category: "Portfolios", src: "/images/categories/portfolios.png" },
 ];
+
+const sliderItems = [
+  {
+    id: 1,
+    category: "Mobile App Designs",
+    src: "/images/slider/logo1.png",
+  },
+  {
+    id: 2,
+    category: "Mockups Designs",
+    src: "/images/slider/logo2.png",
+  },
+  {
+    id: 3,
+    category: "Banners/Flyers",
+    src: "/images/slider/logo3.png",
+  },
+  { id: 4, category: "Logos", src: "/images/slider/logo4.png" },
+  {
+    id: 5,
+    category: "Visiting Cards",
+    src: "/images/slider/logo5.png",
+  },
+  { id: 6, category: "Wireframes", src: "/images/slider/logo6.png" },
+  { id: 7, category: "Dashboard", src: "/images/slider/logo7.png" },
+  { id: 8, category: "Portfolios", src: "/images/slider/logo8.png" },
+];
 export default function Home() {
   return (
     <section className="w-full ">
       <HeroSection />
       <FilterComponent />
-      <Sliders  images1={items} images2={items}/>
+      <Sliders images1={sliderItems} images2={sliderItems} />
       <LeftRightImgForm
         title="Select What You"
         coloredText="Want"
